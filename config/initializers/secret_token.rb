864f71d17d5a318acd718fc781980a5610ecf51c
Chrissy Clark <chris@rapgenius.com>
2014-11-22 20:11:42 -0500

    mew

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GimmePresents::Application.config.secret_key_base = '96a788a81ad8598a11993c77a3a2279778eeeeadb8329e3dcff8484beeb79fa6ca6062f462a0c689ebd27380f169c824c28acadb0c0a63feeab406f263d8cca5'
