b0634c37b72802fd9893a4fac42da6a0b0f2ec48
chrissy <chris@genius.com>
2015-12-02 20:30:45 -0600

    better help text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,11 @@ module ApplicationHelper
       current_user.try(:authenticated?)
     end
 
-    def button_text_for_user_auth
-      if current_user
+    def button_text_for_user_auth(list)
+      if can_edit(list)
         "Save this list with my Google account"
       else
-        "Login with Google to edit this list"
+        "Login with Google to edit your list"
       end
     end
 end
